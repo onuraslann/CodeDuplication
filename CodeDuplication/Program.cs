35c489271808bc4cb01c56bb5453b98cb72160fd
Onuraslann <theeaslann1@gmail.com>
2022-05-26 14:41:41 +0300

    Add project files.

---
+++ CodeDuplication/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CodeDuplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bir sayi giriniz");
+            //var number = Convert.ToInt32(Console.ReadLine());
+           
+            //if(number == 10)
+            //{
+            //    Console.WriteLine("Sayının değeri : " + number);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Tekrar deneyiniz");
+            //}
+            //switch (number)
+            //{
+            //    case 10:
+            //        Console.WriteLine("sayının değeri 10");
+            //        break;
+            //    case 20:
+            //        Console.WriteLine("sayının değeri 20");
+            //        break;
+            //    default:
+            //        Console.WriteLine("tekrar deneyiniz");
+            //        break;
+            //}
+
+        }
+       
+    }
+}
---
+++ Method/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Method
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+           Add(20,30);
+            Add(70);
+            Add2();
+        }
+        public static void Add(int a ,int b=80)
+        {
+            var number = a + b;
+            Console.WriteLine(number);
+        }
+        public static int Add2()
+        {
+           var a = Convert.ToInt32(Console.ReadLine());
+            var b= Convert.ToInt32(Console.ReadLine());
+            var number = a + b;
+            return number;
+        }
+    }
+}
