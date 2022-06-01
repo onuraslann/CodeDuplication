60ec59d952ee288c3eb96da015f59a8820a1bba2
Onuraslann <theeaslann1@gmail.com>
2022-06-01 13:02:00 +0300

    commit

---
+++ EntityFrameworkDemo/ETradeContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkDemo
+{
+    public class ETradeContext:DbContext
+    {
+      
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"server=(localdb)\MSSQLLocalDB;initial catalog=ETrade;integrated security=true");
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+    
+}
---
+++ EntityFrameworkDemo/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntityFrameworkDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ProductDal _productDal = new ProductDal();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dgwProducts.DataSource = _productDal.GetList();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            _productDal.Add(new Product
+            {
+                 Name=tbxName.Text,
+                 UnitPrice=Convert.ToDecimal(tbxPrice.Text),
+                  UnitInStock=Convert.ToInt32(tbxStock.Text)
+            });
+            dgwProducts.DataSource = _productDal.GetList();
+        }
+
+        private void dgwProducts_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dgwProducts_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            tbxNameUpdate.Text = dgwProducts.CurrentRow.Cells[1].Value.ToString();
+            tbxPriceUpdate.Text = dgwProducts.CurrentRow.Cells[2].Value.ToString();
+            tbxStockUpdate.Text = dgwProducts.CurrentRow.Cells[3].Value.ToString();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            _productDal.Update(new Product
+            {
+                Id = Convert.ToInt32(dgwProducts.CurrentRow.Cells[0].Value),
+                Name = tbxNameUpdate.Text,
+                UnitPrice = Convert.ToDecimal(tbxPriceUpdate.Text),
+                UnitInStock = Convert.ToInt32(tbxStockUpdate.Text)
+            });
+            dgwProducts.DataSource = _productDal.GetList();
+        }
+         
+
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            _productDal.Delete(new Product
+            {
+                Id = Convert.ToInt32(dgwProducts.CurrentRow.Cells[0].Value)
+
+            });
+            dgwProducts.DataSource = _productDal.GetList();
+        }
+    }
+}
